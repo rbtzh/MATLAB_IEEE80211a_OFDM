2a2fefbee75b42f609738bfdabb92a894766511b
Zhao Yanbo <hi@zhaoyanbo.com>
2024-03-28 21:51:53 +0800

    apply subclass to data and signal

--- lib/PPDU/DATA.m
+++ lib/PPDU/DATA.m
@@ -1,16 +1,9 @@
-classdef DATA
+classdef DATA < FIELD
     %DATA Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-        bin
         scrambled
-        convoluted
-        interleaved
-        modulated
-        piloted
-        ifft64ed
-        cyclic_prefixed
     end
     
     methods
@@ -109,73 +102,6 @@ classdef DATA
             elseif code_rate==3/4
                 obj.modulated = QAM16(in);                                %QAM
             end
-        end
-
-        function obj = pilot(obj, in)
-            PilotScramble=[1 1 1 1 -1 -1 -1 1 -1 -1 -1 -1 1 1 -1 1 -1 -1 1 1 -1 1 1 -1 1 1 1 1 ...
-                  1 1 -1 1 1 1 -1 1 1 -1 -1 1 1 1 -1 1 -1 -1 -1 1 -1 1 -1 -1 1 -1 -1 1 1 1 1 1 -1 -1 1 ...
-                  1 -1 -1 1 -1 1 -1 1 1 -1 -1 -1 1 1 -1 -1 -1 -1 1 -1 -1 1 -1 1 1 1 1 -1 1 -1 1 -1 1 -1 ...
-                  -1 -1 -1 -1 1 -1 1 1 -1 1 -1 1 1 1 -1 -1 1 -1 -1 -1 1 1 1 -1 -1 -1 -1 -1 -1 -1];%导频伪随机序列
-            NumDataSubc=48;% 数据子载波数
-            NumSubc=52;%子载波数
-            DataSubcPatt=[1:5 7:19 21:26 27:32 34:46 48:52]';%数据子载波的位置
-            PilotSubcPatt=[6 20 33 47]';%导频子载波的位置
-            NumPilotSubc=4;% 导频子载波数
-            PilotSubcSymbols=[1;1;1;-1];%导频子载波的符号
-            
-            n_mod_syms = size(in,2);%发送的映射调制符号数
-            n_ofdm_syms = ceil(n_mod_syms/NumDataSubc);%发送的OFDM符号数
-            in=[in zeros(1,n_ofdm_syms*NumDataSubc-n_mod_syms)];
-            
-            %导频加扰模式 
-            scramble_patt = repmat(PilotScramble,1,ceil(n_ofdm_syms/length(PilotScramble)));%重复导频扰码，使得其长度至少与OFDM符号数一样
-            scramble_patt = scramble_patt(1:n_ofdm_syms);%截取与OFDM符号数个导频扰码序列
-            
-            mod_ofdm_syms = zeros(NumSubc, n_ofdm_syms);
-            mod_ofdm_syms(DataSubcPatt,:) = reshape(in, NumDataSubc, n_ofdm_syms);%将映射调制符号mod_syms按分组插入NumDataSubc个数据子载波中
-            
-            %将导频子载波符号序列进行扰码后插入相应导频子载波位置上
-            mod_ofdm_syms(PilotSubcPatt,:) = repmat(scramble_patt, NumPilotSubc,1).*repmat(PilotSubcSymbols, 1, n_ofdm_syms);
-            %导频插入后的数据输出
-            obj.piloted = mod_ofdm_syms(:).';
-        end
-
-        function obj = IFFT64(obj, in)
-            %IFFT64：信号由频域变换到时域
-            UsedSubcIdx=[7:32 34:59]';%使用的子载波索引
-            NumSubc=52;%子载波数
-            
-            %OFDM符号数
-            num_symbols =size(in, 2)/NumSubc;
-            
-            %将数据加到相应子载波上，并在未使用的子载波上补0
-            syms_into_ifft = zeros(64, num_symbols);
-            syms_into_ifft(UsedSubcIdx,:) = reshape(in(:),NumSubc, num_symbols);
-             
-            %数据位置置换
-            resample_patt=[33:64 1:32];
-            syms_into_ifft(resample_patt,:) = syms_into_ifft;
-               
-            % 变换到时域
-            time_syms = zeros(1,num_symbols*64);%初始化time_syms
-            ifft_out = ifft(syms_into_ifft);
-            time_syms(1,:) = ifft_out(:).';
-            obj.ifft64ed = time_syms;
-        end
-
-        function obj = cyclic_prefix(obj, in)
-            num_symbols = size(in, 2)/64;      %需要传输的符号数（每符号有64数据码元）
-            time_signal = zeros(1, num_symbols*80);   %产生时域信号的初始状态；
-            
-            % 增加循环前缀
-            symbols = reshape(in(:), 64, num_symbols);
-            tmp_syms = [symbols(49:64,:); symbols];    %取一个符号后16bit
-            tmp_syms(1,:)=tmp_syms(1,:)*0.5;           %对符号的首尾进行加窗处理
-            tmp_syms_end(1,:)=symbols(1,:)*0.5;
-            
-            tmp_syms(1,2:num_symbols)=tmp_syms(1,2:num_symbols)+tmp_syms_end(1:num_symbols-1);
-            time_signal(:) = tmp_syms(:).';
-            obj.cyclic_prefixed = time_signal;
         end
 
     end
--- main.m
+++ main.m
@@ -5,56 +5,88 @@ addpath(genpath('./lib'))
 
 disp('#############################################');
 disp("802.11a OFDM Simulation implemented in MATLAB");
-disp('#############################################\n\n');
+disp('#############################################');
 
 %% 初始化
-% Get some essential value from user:
+% initialization
+% Get some essential value from user using dialog
 [trans_rate,message,scrambling_seed] = showDialog();
 
 
 %% 信源 MAC地址配置 MAC层成帧 SERVICE FIELD生成
+% calculation of MAC layer data passed to PHY layer
 % Do someting rate specified configuration
 [RATE, code_rate, Pad_Bits, Tail, SERVICE] = rateHandler(trans_rate);
 
 %% 生成 PSDU
+% PSDU construction
 psdu = PSDU(message);
 
 %% 生成 DATA 段
+% DATA field/object construction
 data = DATA(SERVICE, psdu.bin, Tail, Pad_Bits);
+
 %% DATA 加扰
+% DATA field scrambling
 data = data.scrambler(data.bin, scrambling_seed);
+
 %% DATA 卷积编码
-%TODO change the implementation of this
+% DATA field convolution encoding
 data = data.convolver_tx(data.scrambled, code_rate);
+
 %% DATA 交织编码
+% DATA field interleaved encoding
 data = data.interleaver_tx(data.convoluted, code_rate);
+
 %% DATA 调制
+% DATA field Modulation
 data = data.modulator(data.interleaved, code_rate);
+
 %% DATA 添加导频
+% DATA field add pilot 
 data = data.pilot(data.modulated);
+
 %% DATA 进行 IFFT64 运算
+% DATA field do IFFT64 calculation
 data = data.IFFT64(data.piloted);
+
 %% DATA 添加循环前缀
+% DATA field add cyclic prefix
 data = data.cyclic_prefix(data.ifft64ed);
 
 %% 生成 SIGNAL 段
+% SIGNAL field/object construction
 signal = SIGNAL(RATE, psdu.length);
+
 %% SIGNAL 卷积编码
+% SIGNAL field convolution encoding
 signal = signal.convolver_tx(signal.bin);
+
 %% SIGNAL 交织编码
+% SIGNAL field interleaved encoding
 signal = signal.interleaver_tx(signal.convoluted);
+
 %% SIGNAL 调制
+% SIGNAL field Modulation
 signal = signal.modulator(signal.interleaved);
+
 %% SIGNAL 添加导频
+% SIGNAL field add pilot 
 signal = signal.pilot(signal.modulated);
+
 %% SIGNAL 进行 IFFT64 运算
+% SIGNAL field do IFFT64 calculation
 signal = signal.IFFT64(signal.piloted);
+
 %% SIGNAL 添加循环前缀
+% SIGNAL field add cyclic prefix
 signal = signal.cyclic_prefix(signal.ifft64ed);
 
 %% 生成 PREAMBLE 段
+% PREAMABLE field/object construction
 preamable = PREAMBLE();
 
 %% 生成 PPDU
+% PPDU construction
 ppdu = PPDU(preamable, signal, data);
 
