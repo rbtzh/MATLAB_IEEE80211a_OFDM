c698e9709e8678960f8acfe16a2cf03c5cd6f827
Zhao Yanbo <hi@zhaoyanbo.com>
2024-03-28 20:12:53 +0800

    start implementation of signal

--- main.m
+++ main.m
@@ -12,8 +12,7 @@ disp('#############################################\n\n');
 [trans_rate,message,scrambling_seed] = showDialog();
 
 
-%% 信源 MAC地址配置 MAC层成帧 SERVICE field生成
-
+%% 信源 MAC地址配置 MAC层成帧 SERVICE FIELD生成
 % Do someting rate specified configuration
 [RATE, code_rate, Pad_Bits, Tail, SERVICE] = rateHandler(trans_rate);
 
@@ -29,21 +28,38 @@ data = DATA(SERVICE, psdu.bin, Tail, Pad_Bits);
 
 %% DATA 加扰
 data = data.scrambler(data.bin, scrambling_seed);
+
 %% DATA 卷积编码
 %TODO change the implementation of this
 data = data.convolver_tx(data.scrambled, code_rate);
+
 %% DATA 交织编码
 data = data.interleaver_tx(data.convoluted, code_rate);
+
 %% DATA 调制
 data = data.modulator(data.interleaved, code_rate);
+
 %% DATA 添加导频
 data = data.pilot(data.modulated);
+
 %% DATA 进行 IFFT64 运算
 data = data.IFFT64(data.piloted);
+
+%% DATA 添加循环前缀
+data = data.cyclic_prefix(data.ifft64ed);
 
 %% SIGNAL FIELD生成
 % SIGNAL used in PPDU
 signal = SIGNAL(RATE, psdu.length);
+
+%% SIGNAL 卷积编码
+
+%% SIGNAL 交织编码
+%% SIGNAL 调制
+%% SIGNAL 添加导频
+%% SIGNAL 进行 IFFT64 运算
+%% SIGNAL 添加循环前缀
+
 %% PREAMBLE产生
 % Preamable used in PPDU
     % preamable.short
